2667deab6a262fcd001ad077172fbcb35ed3c98c
blooper20 <70786167+blooper20@users.noreply.github.com>
2022-01-19 18:02:51 +0900

    first scene	designed

--- MatJipSoGae/MyTableViewCell.swift
+++ MatJipSoGae/MyTableViewCell.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class MyTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var myLable: UILabel! // Oulet 변수로 myLable 선언
+    @IBOutlet weak var nameLbl: UILabel! // 매장이름 라벨 연결
+    @IBOutlet weak var categoryLbl: UILabel! // 음식 종류 라벨 연결
+    @IBOutlet weak var restImg: UIImageView! // 매장 사진 이미지 연결
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- MatJipSoGae/ViewController.swift
+++ MatJipSoGae/ViewController.swift
@@ -24,22 +24,25 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     func numberOfSections(in tableView: UITableView) -> Int { // 섹션 함수
         return 3 // 섹션의 개수를 3개로 지정
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // 행(row, cell) 관련 함수
-//        let cell = UITableViewCell.init(style: .value1, reuseIdentifier: "myCell")
-//        // let cell = UITableViewCell.(style: .default, reuseIdentifier: "myCell")
-//        // UITableViewCell의 init생성자의 style을 subtitle 값으로 생성한 후 상수 cell로 선언
-//        cell.textLabel?.text = "\(indexPath.row)"  // indexPath의 row값을 cell의 textLabel에 출력
-//        cell.detailTextLabel?.text = indexPath.description
-//        // cell의 detailTextLabel값을 indexPath의 description값으로 설정
-//        // description의 값에 앞은 섹션, 뒤의 값은 섹션의 행을 뜻한다.
-//        cell.imageView?.image = UIImage(named: "swift.png") // cell에 image를 삽입
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell // cell의 아이디가 "myCell"인 것을 사용
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 행(row, cell) 관련 함수
+        // let cell = UITableViewCell.init(style: .value1, reuseIdentifier: "myCell")
+        // let cell = UITableViewCell.(style: .default, reuseIdentifier: "myCell")
+        // UITableViewCell의 init생성자의 style을 subtitle 값으로 생성한 후 상수 cell로 선언
+        // cell.textLabel?.text = "\(indexPath.row)"  // indexPath의 row값을 cell의 textLabel에 출력
+        // cell.detailTextLabel?.text = indexPath.description
+        // cell의 detailTextLabel값을 indexPath의 description값으로 설정
+        // description의 값에 앞은 섹션, 뒤의 값은 섹션의 행을 뜻한다.
+        // cell.imageView?.image = UIImage(named: "swift.png") // cell에 image를 삽입
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+        // cell의 아이디가 "myCell"인 것을 사용
         // as를 사용해서 부모를 자식으로 바꾼다. =DownCasting
-        cell.myLable.text = indexPath.description // myLable의 text를 indexPath.description로 초기화
+        cell.nameLbl.text = indexPath.description // nameLbl의 text를 indexPath.description로 초기화
+        cell.categoryLbl.text = "양식"   // categoryLbl의 text를 "양식"으로 초기화
         return cell  // cell값을 리턴
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {  // 행(cell)을 클릭했을 때
+        print(indexPath.row) // 콘솔 창에 행을 출력
     }
 }
 
